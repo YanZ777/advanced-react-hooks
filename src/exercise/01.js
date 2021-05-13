9268cfee185168084e75d8aae940ae43b314e8fd
Yan <yanzhao777@gmail.com>
2021-05-13 01:00:27 -0700

    Complete Exercise 1, Extra Credit 4
    - use reducer s per convention

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -3,8 +3,13 @@
 
 import * as React from 'react'
 
-const stateReducer = (previousState, callback) => {
-   return callback(previousState);
+const stateReducer = (previousState, dispatch) => {
+   const { type, step } = dispatch;
+   switch(type) {
+      case 'INCREMENT':
+         return {count: previousState.count + step};
+      default:
+   }
 }
 
 function Counter({initialCount = 0, step = 1}) {
@@ -12,14 +17,14 @@ function Counter({initialCount = 0, step = 1}) {
   // 💰 React.useReducer(countReducer, initialCount)
   // const [count, setCount] = React.useState(initialCount)
 
-  const [state, setState] = React.useReducer(stateReducer, { count: initialCount });
+  const [state, dispatch] = React.useReducer(stateReducer, { count: initialCount });
   const { count } = state;
 
   // 💰 you can write the countReducer function so you don't have to make any
   // changes to the next two lines of code! Remember:
   // The 1st argument is called "state" - the current value of count
   // The 2nd argument is called "newState" - the value passed to setCount
-  const increment = () => setState(currentState => ({count: currentState.count + step}))
+  const increment = () => dispatch({type: 'INCREMENT', step})
   return <button onClick={increment}>{count}</button>
 }
 
